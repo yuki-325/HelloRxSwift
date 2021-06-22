b8aad9dbeda9beb89706f8073844db4b8992fb0a
中野勇貴 <sakeikura325325@gmail.com>
2021-06-22 20:53:13 +0900

    subscriptionの破棄

--- HelloRxSwift/RxSwiftPlayground2.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground2.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
+import RxSwift
 
-var greeting = "Hello, playground"
+//MARK: - subscriptionの破棄
+
+let disposeBag = DisposeBag()
+
+Observable.of("A", "B", "C")
+    .subscribe {
+        print($0)
+    }.disposed(by: disposeBag)
+
+Observable<String>.create { observar in
+    observar.onNext("A")
+    observar.onCompleted()
+    observar.onNext("?") //onCompletedした後はonNextできない
+    return Disposables.create()
+}.subscribe(onNext: { print($0) }, onError: { print($0) }, onCompleted: { print("Completed") }, onDisposed: { print("Disposed") })
